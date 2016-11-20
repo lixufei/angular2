7671dc368f76f63bd1d8444bd8f033d76bb905df
LiXuFei <xfli@thoughtworks.com>
2016-11-20 23:08:10 +0800

    deep link to hero detail and can go back.

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,27 +8,13 @@ import { HeroDetailComponent } from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
 import { HeroService }   from './hero.service';
 import { DashboardComponent }   from './dashboard.component';
+import {AppRoutingModule} from './app-routing.module'
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            },
-        ])
-
+        AppRoutingModule,
     ],
     declarations: [
         AppComponent,
