d14368dea2077a325ec8d8a8e18cf4ec67901e87
LiXuFei <xfli@thoughtworks.com>
2016-11-20 23:17:39 +0800

    add styles for all components.

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -8,6 +8,7 @@ import { HeroService } from './hero.service';
     moduleId: module.id,
     selector: 'my-hero-detail',
     templateUrl: 'hero-detail.component.html',
+    styleUrls: [ 'hero-detail.component.css' ],
 
 })
 export class HeroDetailComponent implements OnInit{
